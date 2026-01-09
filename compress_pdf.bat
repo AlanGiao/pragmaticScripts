81e89437527bfb8562b393d1362041afda0db067
jmlv <d202480135@hust.edu.cn>
2026-01-09 21:35:43 +0800

    Compress PDF using GhostScript (on Windows).
    The script will compress all the PDFs inside the folder that this script
    lies in. The compressed PDFs will be saved into a folder named CompressedPDF.
    To change it from CompressedPDF to other folders, modify the
    `OUTPUT_DIR` variable in this script.
    
    It uses gswin64c.exe as backend. To use this script,
    you should configure the `GS_PATH` variable to the full path of
    gswin64c.exe.

---
+++ compress_pdf.bat
@@ -0,0 +1,22 @@
+rem Ref1: https://blog.csdn.net/finghting321/article/details/105244088
+rem Ref2: https://juejin.cn/post/6951297944398069773
+@echo off
+set "GS_PATH=C:\Program Files\gs\gs10.06.0\bin\gswin64c.exe"
+set "OUTPUT_DIR=.\CompressedPDF"
+rem Execute program
+
+if not exist %OUTPUT_DIR% (
+    echo %OUTPUT_DIR% do not exists.
+    mkdir %OUTPUT_DIR%
+)
+for %%f in (*.pdf) do (
+    echo Compressing PDF: %%f
+    "%GS_PATH%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%OUTPUT_DIR%\%%f" "%%f"
+    if exist "%OUTPUT_DIR%\%%f" (
+        echo Successed: %%f
+    ) else (
+        echo Failed: %%f
+    )
+)
+
+echo Compressed PDFs have been saved into: %OUTPUT_DIR%
