45f5cf149e4f35aad97b08cb3e7a63c897b649d3
jmlv <d202480135@hust.edu.cn>
2026-01-16 12:19:11 +0800

    refactor(GS) No loger use absolute path of gs

--- compress_pdf.bat
+++ compress_pdf.bat
@@ -1,7 +1,7 @@
 rem Ref1: https://blog.csdn.net/finghting321/article/details/105244088
 rem Ref2: https://juejin.cn/post/6951297944398069773
 @echo off
-set "GS_PATH=C:\Program Files\gs\gs10.06.0\bin\gswin64c.exe"
+set "GS=gswin64c.exe"
 set "OUTPUT_DIR=.\CompressedPDF"
 rem Execute program
 
@@ -11,7 +11,7 @@ if not exist %OUTPUT_DIR% (
 )
 for %%f in (*.pdf) do (
     echo Compressing PDF: %%f
-    "%GS_PATH%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%OUTPUT_DIR%\%%f" "%%f"
+    "%GS%" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%OUTPUT_DIR%\%%f" "%%f"
     if exist "%OUTPUT_DIR%\%%f" (
         echo Successed: %%f
     ) else (
